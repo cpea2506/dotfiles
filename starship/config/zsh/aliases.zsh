6d57187b627a343a6087a69ba1ec4216e946cad2
cpea2506 <vienpro2506@gmail.com>
2022-02-08 00:51:01 +0700

    starship(zsh): remove uneeded aliases

--- starship/config/zsh/aliases.zsh
+++ starship/config/zsh/aliases.zsh
@@ -3,14 +3,9 @@ source $ZDOTDIR/functions.zsh
 
 alias ~=cd
 alias cl=clear 
-alias train=sl
 alias lg=lazygit
 alias neo=neofetch
-alias aqua=asciiquarium
 alias btm="btm -C $HOME/.config/btm/config.toml"
-
-# kitty
-alias icat="kitty +kitten icat"
 
 alias ..="cd .."
 alias nvide="neovide --frameless"
@@ -25,10 +20,6 @@ alias gm="git merge"
 alias gst="git status"
 alias gra="git remote add"
 alias grs="git remote set-url"
-
-# database
-alias postgres-start="postgres -D /opt/homebrew/var/postgres"
-alias mysql-start="mysqld_safe --datadir=/opt/homebrew/var/mysql"
 
 # tree search
 alias ls="exa -a --color=always --icons --group-directories-first"
