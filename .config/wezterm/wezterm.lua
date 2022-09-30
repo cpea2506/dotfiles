ec1a253cfc88296457c3be4cd437b38e92adc931
CPea <vienpro2506@gmail.com>
2022-09-30 17:03:03 +0700

    format wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,14 @@
 local wezterm = require("wezterm")
 
-return {
-	font = wezterm.font("Liga SFMono Nerd Font", { weight = "Bold" }),
-	window_background_opacity = 0.85,
-	window_decorations = "RESIZE",
-	color_scheme = "Dark Flat",
-	hide_tab_bar_if_only_one_tab = true,
+local config = {
 	font_size = 13.0,
 	tab_bar_at_bottom = true,
-	window_padding = {
-		left = "5px",
-		right = "5px",
-	},
+	color_scheme = "Dark Flat",
+	window_decorations = "RESIZE",
+	window_background_opacity = 0.85,
+	hide_tab_bar_if_only_one_tab = true,
+	window_padding = { left = "5px", right = "5px" },
+	font = wezterm.font("Liga SFMono Nerd Font", { weight = "Bold" }),
 }
+
+return config
