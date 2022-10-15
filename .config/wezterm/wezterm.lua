f67dd934813249329c71478db1edf42d2ee3f395
CPea <vienpro2506@gmail.com>
2022-10-15 20:44:48 +0700

    update config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,17 +1,17 @@
 local wezterm = require("wezterm")
 
 local config = {
-	font_size = 13.0,
+	color_scheme = "Dark Flat",
 	cursor_thickness = 2.0,
+	default_cursor_style = "SteadyUnderline",
+	font = wezterm.font("Liga SFMono Nerd Font"),
+	font_size = 13.0,
+	hide_tab_bar_if_only_one_tab = true,
 	tab_bar_at_bottom = true,
 	underline_thickness = 2.0,
-	color_scheme = "Dark Flat",
-	window_decorations = "RESIZE",
 	window_background_opacity = 0.85,
-	hide_tab_bar_if_only_one_tab = true,
-	default_cursor_style = "SteadyUnderline",
+	window_decorations = "RESIZE",
 	window_padding = { bottom = "5" },
-	font = wezterm.font("Liga SFMono Nerd Font"),
 }
 
 return config
