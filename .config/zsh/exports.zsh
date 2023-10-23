cedd9138e231f7323f578185568a7acc20153abe
CPea <cpea2506@gmail.com>
2023-10-24 04:31:48 +0700

    update config

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -1,9 +1,6 @@
 export PATH="$HOME/.local/bin:$PATH"
 export EDITOR="lvim"
 
-# homebrew
-export HOMEBREW_NO_INSTALL_FROM_API=1
-
 # lazygit
 export CONFIG_DIR="$HOME/.config/lazygit"
 
@@ -13,8 +10,3 @@ export DOTBARE_TREE="$HOME"
 
 # fzf
 export FZF_DEFAULT_OPTS="--layout=reverse --color=hl:#c678dd,hl+:#c678dd,info:green,header:blue"
-
-# pnpm
-export PNPM_HOME="$HOME/Library/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-# pnpm end
