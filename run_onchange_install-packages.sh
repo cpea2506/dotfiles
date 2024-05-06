39f7d1ca7f81f16bc35823ca9085ada0a6698588
CPea <cpea2506@gmail.com>
2024-05-06 21:25:41 +0700

    Update install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-printf "Install Brew\n"
+# Brew
+printf "Installing Brew\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-printf "\nInstall Brewfile\n"
+printf "\nInstalling Brewfile and Packages\n"
 brew install rcmdnk/file/brew-file
 brew file set_repo -r git@github.com:cpea2506/Brewfile -y
 brew file install
+
+# Rust
+printf "\nInstalling Rustup and Cargo\n"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
